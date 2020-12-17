76e0f9497e9178d649e029ee15c7d51251f6efa9
cleung0112 <cleung0112@gmail.com>
2020-12-16 23:11:14 -0800

    'add event handlers, simulate a search query'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,34 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Foodie Guide</h1>
-      <SearchBar />
-      <BusinessList /> 
-    </div>
-  );
-}
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'Little Star Pizza',
+  address: '846 Divisadero St',
+  city: 'San Francisco',
+  state: 'CA',
+  zipCode: '94117',
+  category: 'Pizza',
+  rating: 4.6,
+  reviewCount: 840
+};
+
+const businesses = [business,business,business,business,business,business];
+
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Foodie Guide</h1>
+        <SearchBar searchYelp = {this.searchYelp} />
+        <BusinessList businesses = {businesses} /> 
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import './Business.css'
 
-const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'Little Star Pizza',
-    address: '846 Divisadero St',
-    city: 'San Francisco',
-    state: 'CA',
-    zipCode: '94117',
-    category: 'Pizza',
-    rating: 4.6,
-    reviewCount: 840
-};
 
 class Business extends React.Component {
   render() {
@@ -20,17 +9,17 @@ class Business extends React.Component {
         <div className="image-container">
             <img src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=714&q=80' alt='buffalo pizza'/>
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
             <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state} {business.zipCode}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipCode}</p>
             </div>
             <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
             </div>
         </div>
       </div> 
