734f82cc736b3a50a8ed26063e8d9f5796c17116
cleung0112 <cleung0112@gmail.com>
2020-12-19 20:31:10 -0800

    setup node server for yelp api

---
+++ api/routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+
+const apiKey = 'QDFx-SqbZDOGW2TLXRA-RkNXmpFDUYn0GLverKgNf6Y4enSgocZcpe_8wBFduQa5fnqP9T3FDqeGftoe8P_rv74UqtXDGhMeegrnj4Dt8hxUaZCH96sJsYPiA6aTX3Yx';
+
+router.get('/yelp',  async (req, res, next) => {
+    const { term, location, sortBy } = req.query;
+
+    try {
+      const businesses = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        'headers': {
+          'origin': 'http://localhost:5000',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+      }).then( (response) => {
+        return response.json();
+      }).then( (jsonResponse) => {
+        console.log("test2", jsonResponse);
+        if( jsonResponse['businesses'] ) {
+          return jsonResponse['businesses'].map( (business) => { 
+            return {
+              'id': business.id,
+              'imageSrc': business.image_url,
+              'name': business.name,
+              'address': business.location.address1,
+              'city': business.location.city,
+              'state': business.location.state,
+              'zipCode': business.location.zipCode,
+              'category': business.categories && business.categories.length > 0 ? business.categories[0].title : null,
+              'rating': business.rating,
+              'reviewCount': business.review_count
+            }
+          })
+        }
+      });
+
+      res.status(200).json({
+        businesses
+      });
+    } catch (e) {
+      console.log("error", e);
+    }
+});
+
+module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-// import axios from 'axios';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,14 +13,45 @@ class App extends React.Component {
     this.searchYelp = this.searchYelp.bind(this);
   }
 
-  searchYelp(term, location, sortBy) {
-    // axios.get('/api/v1/yelp', { term, location, sortBy }).then((res) => {
-    //   const response = res.data;
+  async searchYelp(term, location, sortBy) {
 
-    //   this.setState({
-    //     'businesses' : response
-    //   })
-    // });
+    // const r = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+    //     'headers': {
+    //       'Authorization': `Bearer ${apiKey}` ,
+    //     },
+    //   }).then( (response) => {
+    //     const json = response.json();
+    //     console.log("test", json);
+    //       return json;
+    //   }).then( (jsonResponse) => {
+    //     console.log("test2", jsonResponse);
+    //       if( jsonResponse['businesses'] ) {
+    //         return jsonResponse['businesses'].map( (business) => { 
+    //           return {
+    //             'id': business.id,
+    //             'imageSrc': business.image_url,
+    //             'name': business.name,
+    //             'address': business.location.address1,
+    //             'city': business.location.city,
+    //             'state': business.location.state,
+    //             'zipCode': business.location.zipCode,
+    //             'category': business.categories[0].title,
+    //             'rating': business.rating,
+    //             'reviewCount': business.review_count
+    //           }
+    //         })
+    //       }
+    //   });
+
+    //   console.log(r)
+
+    axios.get('/api/v1/yelp', { params: { term, location, sortBy } }).then((res) => {
+      console.log(res);
+
+      this.setState({
+        'businesses' : res.data.businesses,
+      })
+    });
   };
 
   render() {
